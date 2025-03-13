b160cb547de8897f7bacf5f9dfa5478bb35185d2
Zia Uddin <ziawc0001@gmail.com>
2025-03-13 23:36:03 +0500

    conditional rendring

--- Fragments/learning-fragments/src/App.jsx
+++ Fragments/learning-fragments/src/App.jsx
@@ -2,10 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  let groceryItems = ["Spoons", "Knives", "Pressure Cookers" , "Trays", "Grinders", "Bucket"];
+  let groceryItems = ["Spoons", "Knives", "Pressure Cookers" , "Trays", "Grinders", "Bucket" ];
+
+  //let groceryItems = [];
+  let emptyMessage =  groceryItems.length === 0 ? <p>Need grocery items</p> : null;
   return (
     <>
       <h2>Grocery Items</h2>
+      {emptyMessage}
       <ul className="list-group">
         {groceryItems.map((item, index) => (
          <li key={index} className="list-group-item">{item}</li>
